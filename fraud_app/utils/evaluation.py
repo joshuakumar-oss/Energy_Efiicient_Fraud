c79bf11c3919366cc9b1c392d1110ba51cdd6566
joshuakumar-oss <joshua.kumar@bsccsh.christuniversity.in>
2026-03-20 12:48:50 +0530

    Delete fraud_app directory

--- fraud_app/utils/evaluation.py
+++
@@ -1,9 +0,0 @@
-from sklearn.metrics import confusion_matrix, classification_report
-
-
-def get_confusion_matrix(y_true, y_pred):
-    return confusion_matrix(y_true, y_pred)
-
-
-def get_classification_metrics(y_true, y_pred):
-    return classification_report(y_true, y_pred, output_dict=True)
